2bf6d93010ce19e68bc097b4deb855997dd0eb1e
landa4 <player.961@gmx.de>
2020-01-27 19:33:00 +0100

    Initial commit

---
+++ Assets/Scripts/AiPatrol.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiPatrol : MonoBehaviour
+{
+
+    public float lookRadius = 10;
+    public float rotationSpeed = 5f;
+
+    private UnityEngine.AI.NavMeshAgent agent;
+    private Animator animator;
+
+    private GameObject target; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.transform.position, transform.position);
+
+        if(distance <= lookRadius)
+        {
+            agent.SetDestination(target.transform.position);
+            animator.SetFloat("Speed", 0.2f);
+
+            if(distance <= agent.stoppingDistance)
+            {
+                FaceTarget();
+            }
+        }
+
+        if (!agent.pathPending)
+        {
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+                {
+                    animator.SetFloat("Speed", 0f);
+                }
+            }
+        }
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.transform.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+    }
+
+    void OnDrawGizmos()
+    {
+        // Display the explosion radius when selected
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
