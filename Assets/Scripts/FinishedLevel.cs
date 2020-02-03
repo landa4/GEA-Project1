b6caf425415cb787a30bfa2556e7adf7ff40f370
landa4 <player.961@gmx.de>
2020-02-03 13:25:38 +0100

    asdfasdf

---
+++ Assets/Scripts/FinishedLevel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishedLevel : MonoBehaviour
+{
+    public GameObject winningtext;
+    private void OnTriggerEnter(Collider other)
+    {
+       if(string.Compare(other.tag, "Player") == 0){
+            other.gameObject.GetComponent<MoveBehaviour>().SetCanMove(false);
+            winningtext.SetActive(true);
+       }
+
+    }
+}
