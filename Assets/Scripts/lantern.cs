506fa6335fff9768027a8e1492796619b6faa8e9
Jaakko <jaakko.j.hautamaki@gmail.com>
2020-02-03 20:22:29 +0000

    added a fireball for our guy.

--- Assets/Scripts/lantern.cs
+++ Assets/Scripts/lantern.cs
@@ -5,20 +5,27 @@ using UnityEngine;
 public class lantern : MonoBehaviour
 {
     public Light light;
+    public float flicker_interval;
+    float counter;
     // Start is called before the first frame update
     void Start()
     {
-
+        counter = 0;
         light.range = 30;
         light.intensity = 2;
-        light.color = Color.magenta;
+        light.color = Color.red;
     }
 
     // Update is called once per frame
     void Update()
     {
-        light.intensity = Random.Range(1f, 2f);
-        light.range = 15 + light.intensity * 5;
-        light.color = Random.Range(0.1f, 0.4f) * Color.yellow + Random.Range(0.3f, 0.6f) * Color.red;
+        counter += Time.deltaTime;
+
+        if(counter > flicker_interval) {
+            light.intensity = Random.Range(1f, 2f);
+            light.range = 15 + light.intensity * 5;
+            light.color = Random.Range(0.1f, 0.4f) * Color.yellow + Random.Range(0.3f, 0.6f) * Color.red;
+            counter -= flicker_interval;
+        }
     }
 }
