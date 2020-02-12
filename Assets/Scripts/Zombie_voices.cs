fb2cb1c07163bf8ae07601d2114dd440582403d3
Jaakko <jaakko.j.hautamaki@gmail.com>
2020-02-12 19:18:58 +0000

    added a reset scene script and tweaked the zombie sounds reaction.

--- Assets/Scripts/Zombie_voices.cs
+++ Assets/Scripts/Zombie_voices.cs
@@ -8,27 +8,31 @@ public class Zombie_voices : MonoBehaviour
     public List<AudioSource> aggressiveSounds;
     public List<AudioSource> passiveSounds;
     private AudioSource lastPlaying;
+    private bool lastSoundIsAggressive;
     // Start is called before the first frame update
     void Start()
     {
         aggressive = false;
         lastPlaying = passiveSounds[0];
+        lastSoundIsAggressive = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         if(aggressive) {
-            if(!lastPlaying.isPlaying) {
+            if(!lastPlaying.isPlaying || !lastSoundIsAggressive) {
                 int i = Random.Range(0, aggressiveSounds.Count);
                 lastPlaying = aggressiveSounds[i];
                 lastPlaying.Play();
+                lastSoundIsAggressive = true;
             }
         } else {
             if(!lastPlaying.isPlaying) {
                 int i = Random.Range(0, passiveSounds.Count);
                 lastPlaying = passiveSounds[i];
                 lastPlaying.Play();
+                lastSoundIsAggressive = false;
             }
         }
     }
